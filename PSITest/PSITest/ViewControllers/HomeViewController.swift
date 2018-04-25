d24f33496f6a6a0bafca7d6752ec8b385f5244eb
Kajal <kajalluthra01@gmail.com>
2018-04-25 19:23:02 +0800

    Added pins on Map using HomeManager

--- PSITest/PSITest/ViewControllers/HomeViewController.swift
+++ PSITest/PSITest/ViewControllers/HomeViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 import GoogleMaps
 
 class HomeViewController: UIViewController, GMSMapViewDelegate {
-
+    
+    fileprivate var manager: HomeManager!
     fileprivate var mapView: GMSMapView!
     
+    //MARK:- App Life Cycle Method
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         setupUI()
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     //MARK:- Private Method
     func setupUI(){
         
@@ -29,16 +36,27 @@ class HomeViewController: UIViewController, GMSMapViewDelegate {
         self.mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = self.mapView
         self.mapView.delegate = self
-  
         
+        self.manager = HomeManager.init()
+        self.manager.delegate = self
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addMarkersOnMap(){
+        
+        let path = GMSMutablePath()
+        for pins in self.manager.location_array {
+            let coordinates = CLLocationCoordinate2D(latitude: pins.lat, longitude: pins.lng)
+            
+            let marker = GMSMarker()
+            marker.position = coordinates
+            marker.icon = UIImage(named: "pin")
+            marker.appearAnimation = GMSMarkerAnimation.pop
+            
+            marker.map = mapView
+            path.add(coordinates)
+        }
     }
-
-
+    
 }
 
 //MARK:- GMSMapViewDelegate
@@ -55,7 +73,7 @@ extension HomeViewController {
 extension HomeViewController : HomeManagerDelegate {
     
     @objc internal func didUpdateHomeMarkers() {
-        //update pins
+        self.addMarkersOnMap()
     }
     
     @objc internal func didUpdateErrorWithAlert( error: String){
