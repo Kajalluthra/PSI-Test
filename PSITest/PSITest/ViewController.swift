50a689cc0f2db84452b971d25d6ce132ef6512be
Kajal <kajalluthra01@gmail.com>
2018-04-25 16:17:54 +0800

    Initial commit :- Created project with Swift

---
+++ PSITest/PSITest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PSITest
+//
+//  Created by Kajal on 25/4/2018.
+//  Copyright © 2018 Kajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
