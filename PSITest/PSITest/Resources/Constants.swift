038d48c94064fa0d4f4d40f599cb8ff1ac016154
Kajal <kajalluthra01@gmail.com>
2018-04-25 17:29:16 +0800

    Added Google Map API Key + Constants file

---
+++ PSITest/PSITest/Resources/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  PSITest
+//
+//  Created by Kajal on 25/4/2018.
+//  Copyright © 2018 Kajal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    
+    static let googleKey = "AIzaSyDXCSyTi654n6HbdCSKiZhh-kfYilT3OFo"
+
+}
